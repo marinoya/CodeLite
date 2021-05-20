5e13eab6e68e0ee6e8afcbb5bb96628fc692807d
marinoya <marinov.yasen@gmail.com>
2021-05-20 22:04:16 +0300

    saved

--- OOP/OOP20May2021/main.cpp
+++ OOP/OOP20May2021/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //Task 1 - Create a game hero class- at least 3 datat members(at least one should be private), constuctors(at least 3 - one of aech type)
 //setter and getter methods, destructor - use this keyword when possible
-
+//Task 2 - create at least 4 operator overloading options for your class
 class Cleric
 	{
 private:
@@ -81,11 +81,39 @@ public:
 			res.i = this -> i + obj1.i;
 			return res;
 		}
+		
+		void operator+ (int obj1)
+		{
+			cout<< this -> i + obj1;
+		}
+		
+		void operator* (int a)
+		{
+			cout<< this -> i * a;
+		}
+		
+		void operator< (int a)
+		{
+			cout<< this -> i - a;
+		}
+		
+		void operator> (int a)
+		{
+			cout<< this -> i + a;
+		}
+		
 	};
 	
 
 
 int main (){
 	cout << "Hello world" << endl;
+	
+	MyClass obj1;
+	MyClass obj2;
+	MyClass obj3 = obj1 + obj2;
+	MyClass obj4 = obj1 + 5;
+	
+	
 	return 0;
 	}
